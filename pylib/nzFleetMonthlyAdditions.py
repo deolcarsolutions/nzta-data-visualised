3c81d5c5512d3bd00946bceb896dcf336a0b4494
deolcarsolutions <wreckers@deolcarsolutions.co.nz>
2021-05-01 00:51:38 +1200

    separate script for vehicle additions

---
+++ pylib/nzFleetMonthlyAdditions.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+filePath = Path(__file__)
+yearMonth = "202103"
+baseName = "additions-to-the-national-vehicle-fleet"
+ext = '.csv'
+baseFileName = Path(Path(__file__).parent.parent, "data",
+                    yearMonth, baseName)
+
+tableName = 'table6'
+fileName = baseFileName / (tableName+ext)
+table6 = pd.read_csv(fileName,
+                     header=0)
+print(fileName)
+print(table6)
+# Get columns using regular expressions
+# print(table6.filter(regex=r"(?i)mar"))
+print(table6.filter(regex=r"(?i){:}".format("mar")))
+#table3.dropna(axis=1, inplace=True)
+
+
+def pickSubCols(df, colName):
+    subCols = df.filter(regex=r"(?i){:}".format(colName))
+    # Get First row as a dictionary
+    firstRow = dict(subCols.loc[0])
+    # swap key, values in firstRow
+    # colNames = dict((v, k) for k, v in firstRow.items())
+    #subCols = subCols[1:].rename(columns=colNames)
+    subCols = subCols[1:].rename(columns=firstRow)
+    return subCols
+
+
+def top10Cars(df, month, pmonth):
+    """
+    df: Table contains month as first row and vehicle make, 
+        count of vehicles as second row 
+    """
+    currentMonth = pickSubCols(df, month)
+    pastMonth = pickSubCols(df, pmonth)
+
+    print("make, ccount, count changep, position change")
+    cpos = 1
+    for cmake, ccount in zip(currentMonth['Vehicle make'], currentMonth['Count of vehicles']):
+        ccount = int(ccount)
+        if cmake in pastMonth['Vehicle make'].values:
+            row = pastMonth.loc[pastMonth['Vehicle make'] == cmake]
+            ppos = row.iloc[0].name
+            pcount = int(row['Count of vehicles'])
+            print(cmake, ccount, "{:} ({:.1f}%)".format(ccount-pcount, (ccount-pcount)/pcount*100),
+                  ppos-cpos)
+        else:
+            print(cmake, ccount)
+        cpos += 1
+
+    return currentMonth, pastMonth
+
+
+c, p = top10Cars(table6, 'mar', 'feb')
+# print(table1)
+#table1.to_csv("../data/"+yearmonth+"/"+basename+"-table1.csv", index=False)
+# table2.to_csv("../data/"+yearmonth+"/table2.csv", index=False)
+# table3.to_csv("../data/"+yearmonth+"/table3.csv", index=False)
+
+# table1.to_html("../data/"+yearmonth+"/table1.html", index=False)
+#fig1, ax1 = plt.subplots()
+# ax1.pie(table1["num"], labels=table1["vehicleType"],
+#        autopct='%1.1f%%', shadow=True)
+# ax1.legend()
+# ax1.axis('equal')
+# plt.show()
